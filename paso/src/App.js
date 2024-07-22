02b4eb40c9d58ba5bade9794395663b94dac0b61
Ahmed A.A. Kh <ahmedakheirallah@gmail.com>
2024-07-22 12:18:09 +0400

    Worked a bit on the sign in page

--- paso/src/App.js
+++ paso/src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import "./sections/LoginHeader"
 import LoginHeader from './sections/LoginHeader';
+import LoginBody from "./sections/LoginBody"
 
 function App() {
   return (
     <div className="App">
         <LoginHeader/>
+        <LoginBody/>
     </div>
   );
 }
--- paso/src/sections/LoginBody.js
+++ paso/src/sections/LoginBody.js
@@ -1,14 +1,35 @@
 import React from "react";
 import "./Login.css"
+import logo from "../resources/paso-logo.png"
+
+
 let LoginBody = () => {
     return(
         <div className="login-body">
             <div className="login-section">
-                
+                <img className="paso-logo dark-logo" src={logo} alt="Paso dark logo"/>
+                <div className="login-text">
+                        <h1>Welcome Back!</h1>
+                        <p>Please enter your details.</p>
+                </div>
+                <div className="login-div">
+                    <div className="input-div">
+                        <p>Email:</p>
+                        <input type="text" id="email" name="email"/>
+                    </div>
+                    <div className="input-div">
+                        <p>Password:</p>
+                        <input type="text" id="password" name="password"/>
+                    </div>
+                </div>
+                <div className="sign-div">
+                    <input type="button" id="signin" />
+                </div>
             </div>
             <div className="info-section">
-
+                <p>d</p>
             </div>
         </div>
     );
 }
+export default LoginBody;
